171b9684d86e74a0dd33f79b4055a487b5af2022
Igor Savenko <csigorek@gmail.com>
2022-01-23 07:07:29 +0200

    Added servers collection and get_all_servers

--- gateway/api/database.py
+++ gateway/api/database.py
@@ -10,9 +10,13 @@ class Database:
         self.client = pymongo.MongoClient(os.getenv("database"))
         self.database = self.client["DenSec"]
         self.tasks = self.database["tasks"]
+        self.servers = self.database["servers"]
     
     def create_task(self, task):
         return self.tasks.insert_one(task)
     
     def get_task_list(self):
         return list(self.tasks.find({}))
+
+    def get_all_servers(self):
+        return list(self.servers.find({}))
