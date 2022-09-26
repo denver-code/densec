7a7ab663dfb512ff9231c2398bc525c048e97941
denver-code <csigorek@gmail.com>
2022-09-26 15:39:29 +0100

    Merge task names from snake_case -> camelCase

--- gateway/api/task_handler.py
+++ gateway/api/task_handler.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 class Task():
     def __init__(self):
-        self.avaible_tasks = ["ping", "getservers", "generate_uuid"]
+        self.avaible_tasks = ["ping", "getServers", "generateUUID"]
         self.SECRET_KEY = os.getenv("SECRET_KEY")
 
 
@@ -29,20 +29,20 @@ class Task():
     @client_only
     def task_getservers(self, logging, socks, parsed_data):
         '''
-        Sample of request - {"type":"client","task":"getservers"}
+        Sample of request - {"type":"client","task":"getServers"}
         '''
         servers = Database().get_all_servers()
         if servers:
             msg = {"statuscode":200,"data":{"servers":servers}}
         else:
-            msg = {"statuscode":204,"message":"No active servers :("}
+            msg = {"statuscode":204,"message":"No active servers"}
         logging.info(f"{socks.getpeername()[0]} - {msg=}")
         return msg
 
 
     def task_generate_uuid(self, logging, socks, parsed_data):
         '''
-        Sample of request - {"type":"client","task":"generate_uuid"}
+        Sample of request - {"type":"client","task":"generateUUID"}
         '''
         uuid4 = str(uuid.uuid4())
         logging.info(f"{socks.getpeername()[0]} generate {uuid4=}")
@@ -51,7 +51,7 @@ class Task():
 
     def task_ping(self, logging, socks, parsed_data):
         ''' 
-        Sample of request - {"type":"client","task":"serverping"}
+        Sample of request - {"type":"client","task":"ping"}
         '''
         msg = {"statuscode":200,"message":"pong!"}
         logging.info(f"{socks.getpeername()[0]} - {msg=}")
@@ -62,7 +62,7 @@ class Task():
         if "type" in parsed_data :
             if "task" in parsed_data:
                 if parsed_data["task"] in self.avaible_tasks: 
-                    return self.dynamic_call(parsed_data['task'], logging, socks, parsed_data)
+                    return self.dynamic_call(parsed_data['task'].lower(), logging, socks, parsed_data)
                 else:
                     errmsg = {"statuscode":404, "reason":"Command not found! You want use unknown task."}
                     logging.error(f"{socks.getpeername()[0]} - {errmsg=}")
